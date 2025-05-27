21f86e60568d380ed03ede12852000932adad0dc
Jason <132189550+Jezter0@users.noreply.github.com>
2025-05-27 14:34:30 +0700

    ngrok added

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template
+from flask_ngrok import run_with_ngrok
 import joblib
 import re
 import string
 import os
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
 # Paths
 BASE_DIR = os.path.dirname(__file__)
